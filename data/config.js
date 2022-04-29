d8a4b084ae63e120f6dfc6b33e0df6bb2d4aa653
Ian <60456400+KaboomB52@users.noreply.github.com>
2022-04-28 21:09:38 -0500

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,57 +1,60 @@
 export const meta = {
-  title: 'LeBron James - Web Developer and Designer',
-  description: 'Hey, I\'m LeBron. I\'m a web developer and designer building things people love.'
+  title: 'KaboomB52',
+  description: 'Hey, I\'m KaboomB52. I\'m a java developer making fair pvp not a dream.'
 }
 
 export const hero = {
-  title: "Hey there! I'm LeBron James.",
-  desc: "I'm a web developer and designer building things people love."
+  title: "Hi! I'm Kaboom.",
+  desc: "I\'m a java developer making fair pvp not a dream."
 };
 
 export const projects = {
   title: 'My Projects',
-  desc: "Here are some of my favorite projects that I've worked on.",
+  desc: "Here are some of the projects i've worked on.",
   projects: [
     {
-      title: 'Lakers Official',
-      description: 'Home to the Los-Angeles Lakers',
-      link: 'https://lakers.com',
-      github: ''
+      title: 'MythicalMC Network',
+      description: 'A new server bringing the best of the old and\nthe best of the new together.',
+      link: 'https://mythicalmc.org/',
+      github: 'MythicalMC'
     },
     {
-      title: 'Cool App',
-      description: 'A cool app!',
-      link: 'https://lebronscoolapp.com',
-      github: 'theking-lbj/cool-app'
+      title: 'ForestBukkit',
+      description: 'A development studio, currently working on MythicalMC\'s projects',
+      link: 'https://forestbukkit.us/',
+      github: 'ForestBukkit'
     },
     {
-      title: 'Another app',
-      description: 'Another app!',
-      link: 'https://anotherlebronapp.com',
-      github: 'theking-lbj/another-app'
+      title: 'octo-barnacle (Private)',
+      description: 'A plugin testing sending and recieving data over redis.',
+      link: 'https://github.com/KaboomB52/octo-barnacle',
+      github: 'KaboomB52/octo-barnacle'
     }
   ]
 };
 
 export const stack = {
-  title: 'My Stack',
+  title: 'My Abillities',
   stack: [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'Typescript',
-    'React',
-    'NextJS',
-    'TailwindCSS',
-    'Firebase',
-    'Git'
+    'Java [3+ yrs]',
+    'Kotlin [2 months]',
+    'JavaScript [1 yr]',
+    'NodeJS [1 yr]',
+    'React [6 months]',
+    'NextJS [6 months]',
+    'RubyOnRails [5+ yrs]',
+    'SpringFramework [5 months]',
+    'Git [3+ yrs]',
+    'PhoenixFramework [1 yr]',
+    'Debian Based Linux [7 yrs],
+    'Arch Based Linux [1 yr]
   ]
 };
 
 export const contact = {
   title: 'Contact Me',
-  email: 'lebron@gmail.com',
-  github: 'theking-lbj',
-  dribbble: 'theking-lbj',
-  linkedin: 'theking-lbj'
+  email: 'kaboomb52@outlook.com',
+  github: 'KaboomB52',
+  dribbble: '',
+  linkedin: ''
 };
